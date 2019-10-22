37750c3fee9d5908b0db9e4d0dbd8c1cd6788e3b
Jasmine <jasminerojas102@gmail.com>
2019-10-22 19:28:01 -0400

    add token to the new route

--- front/src/components/Form/resturantForm.js
+++ front/src/components/Form/resturantForm.js
@@ -22,7 +22,7 @@ export default class newResturantForm extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
-        fetch('http://localhost:8080/resturant/new', {
+        fetch('http://localhost:8080/resturant/new/' + localStorage.getItem('token'), {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -31,22 +31,23 @@ export default class newResturantForm extends React.Component {
         }) 
         // need to create token or might be part of the public view 
             .then((rep)=>{return rep.json()})
-            .then( (res) => {console.log(res)})
+            .then( (res) => {console.log(res)
+            })
             .catch((err)=>{console.log(err)})
             console.log(this.state);
     }
     render () {
         return (
             <form className="newForm" onSubmit={(event)=> this.handleSubmit(event)}>
-                <div class="container">
-                    <h1 class="cafeTitle">Enter new Resturant or Cafe</h1>
-                    <input type="text" class="placeName" placeholder="Name of Place" name="title" required onChange={(event)=> this.handleChange(event)}/>
+                <div className="container">
+                    <h1 class="cafeTitle">Enter new Resturant/Cafe</h1>
+                    <input type="text" className="placeName" placeholder="Name of Place" name="title" required onChange={(event)=> this.handleChange(event)}/>
                     
-                    <p><label for="picture" class="picture">Upload a picture:</label></p>
-                    <input type="file" class="newImage" accept="image/png, image/jpeg" name="newImage" onChange={(event)=> this.handleChange(event)}/>           
+                    <p><label for="picture" className="picture">Upload a picture:</label></p>
+                    <input type="file" className="newImage" accept="image/png, image/jpeg" name="newImage" onChange={(event)=> this.handleChange(event)}/>           
                     
-                    <input type="text" class="address" placeholder="Address" name="address" required onChange={(event)=> this.handleChange(event)}/>
-                    <input type="text" class="comment" placeholder="comments" name="comment" required onChange={(event)=> this.handleChange(event)}/>                    
+                    <input type="text" className="address" placeholder="Address" name="address" required onChange={(event)=> this.handleChange(event)}/>
+                    <input type="text" className="comment" placeholder="comments" name="comment" required onChange={(event)=> this.handleChange(event)}/>                    
                     <Button className="createBtn" type="submit">Create</Button>
                 </div>
             </form>   
