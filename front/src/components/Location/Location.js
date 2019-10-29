e48796e19544ceb790b4d37a5e34649235b89fef
Jasmine <jasminerojas102@gmail.com>
2019-10-28 20:55:56 -0400

    add constructor

--- front/src/components/Location/Location.js
+++ front/src/components/Location/Location.js
@@ -1,47 +1,24 @@
 import React from 'react';
-import Comment from '../Comment/Comment';
-import Address from '../Address/Address';
 import {connect} from 'react-redux';
-// import ResturantForm from '../Form/resturantForm';
 import CafeImg from '../../Assets/Images/Friends_cafe.png';
 
 export  class Location extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
 
         this.state = {
-            addresses: [{
-                text: '1234 Main St CA, 12345'
-            }],
-            comments: [{
-                text:'example comment'
-            }],
+            addresses: [],
+            comments: []
         }
     }
     render() {
-        const addresses = this.state.addresses.map((address, index)=>
-            <li key={index}>
-                <Address {...address} />
-            </li>
-        );
-        const comments = this.state.comments.map((comment,index)=>
-            <li key={index}>
-                <Comment {...comment}/>
-            </li>
-    );
         return (
             <div>
                 <h3>{this.props.title}</h3>
                 <ul className="locations">
                     <img src={CafeImg}/>
-                    {addresses}
-                    {comments}
-                    {/* <li>
-                        <AddForm
-                            type="comment"
-                            onAdd={text => this.addComment(text)}
-                        />
-                    </li> */}
+                    <li>{this.props.address}</li>
+                    <li>{this.props.comments}</li>
                 </ul>
             </div>
         );
@@ -51,23 +28,3 @@ Location.defaultProps = {
     title: 'Resturant/Cafe'
 };
 export default connect()(Location);
-
-
-
-
-// export default function Location() {
-//     return (
-//         <div>
-//             <h3>Example Resturant</h3>
-//             <img src={CafeImg}/>
-//             <ul className="Location">
-//                 <li>
-//                     <Address/>
-//                 </li>
-//                 <li>
-//                     <Comment/>
-//                 </li>
-//             </ul>
-//         </div>
-//     );
-// }
