b982e9de6801a8450dff2a21e387582e7755bd08
Jasmine <jasminerojas102@gmail.com>
2019-10-28 20:53:17 -0400

    add comments to reducers

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -23,26 +23,26 @@ export const uncReducer = (state=initialState, action) => {
             })
         });
     }
-    // else if (action.type === actions.ADD_COMMENT) {
-    //     const {text, startId, locationIndex} = action;
-    //     const starts = state.starts;
-    //     const start = Object.assign({}, defaultStart, starts[startId]);
-    //     start.location = start.location.map((location, index) => {
-    //         if (index !== locationIndex) {
-    //             return location;
-    //         }
-    //         return Object.assign({}, location, {
-    //             comments: [...location.comments, {
-    //                 text
-    //             }]
-    //         });
-    //     });
+    else if (action.type === actions.ADD_COMMENT) {
+        const {text, startId, locationIndex} = action;
+        const starts = state.starts;
+        const start = Object.assign({}, defaultStart, starts[startId]);
+        start.location = start.location.map((location, index) => {
+            if (index !== locationIndex) {
+                return location;
+            }
+            return Object.assign({}, location, {
+                comments: [...location.comments, {
+                    text
+                }]
+            });
+        });
 
-    //     return Object.assign({}, state, {
-    //         starts: Object.assign({}, starts, {
-    //             [startId]: start
-    //         })
-    //     });
-    // }
+        return Object.assign({}, state, {
+            starts: Object.assign({}, starts, {
+                [startId]: start
+            })
+        });
+    }
     return state;
 };
