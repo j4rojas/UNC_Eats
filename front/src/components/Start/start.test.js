d3324fcd82807e3f897cf6547a9720481ee86d70
Jasmine <jasminerojas102@gmail.com>
2019-11-02 14:05:58 -0400

    add start test

---
+++ front/src/components/Start/start.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {Start} from './start';
+import Location from '../Location/Location';
+import {addLocation} from '../actions';
+
+describe('<Start/>', () => {
+    let seedLocations = [];
+    beforeAll(() => {
+        for (let i = 0; i < 10; i++) {
+            seedLocations.push({
+                title: `Location ${i}`,
+                comments: []
+            });
+        }
+    });
+
+    it('Renders without crashing', () => {
+        const dispatch = jest.fn();
+        shallow(<Start title="Foo" locations={[]} dispatch={dispatch} />);
+    });
+
+    it('Renders the title', () => {
+        const dispatch = jest.fn();
+        const title = 'Foo';
+        const wrapper = shallow(
+            <Start title={title} locations={[]} dispatch={dispatch} />
+        );
+        expect(wrapper.contains(<h2>{title}</h2>)).toEqual(true);
+    });
+
+    it('Dispatches addLocation from addLocation', () => {
+        const dispatch = jest.fn();
+        const wrapper = shallow(<Start locations={[]} dispatch={dispatch} />);
+        // Ignore any previous calls to dispatch
+        dispatch.mockClear();
+        const instance = wrapper.instance();
+        const title = seedLocations[0].title;
+        instance.addLocation(title);
+        expect(dispatch).toHaveBeenCalledWith(addLocation(title));
+    });
+
+    it('Renders the locations', () => {
+        const dispatch = jest.fn();
+        const wrapper = shallow(
+            <Start locations={seedLocations} dispatch={dispatch} />
+        );
+        const locations = wrapper.find(List);
+        expect(locations.length).toEqual(seedLocations.length);
+        const firstLocation = locations.first();
+        expect(firstLocation.prop('title')).toEqual(seedLocations[0].title);
+    });
+});
