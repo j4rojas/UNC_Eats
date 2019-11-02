0ddee93e38e7247fa16d7f1738c622852d3c719f
Jasmine <jasminerojas102@gmail.com>
2019-11-02 15:15:50 -0400

    add header test file

---
+++ front/src/components/Header/Header.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import Header from '../Header/Header';
+
+describe('<Header />', () => {
+    it('Renders without crashing', () => {
+        shallow(<Header text="Foo" />);
+    });
+    
+    it('Renders the text', () => {
+        const text = 'Foo';
+        const wrapper = shallow(<Header text={text}/>);
+        expect(wrapper.text()).toEqual(text);
+    });
+});
