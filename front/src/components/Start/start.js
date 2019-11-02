83700eae2f97bcfd5856e65bb3efcf52e5dccaae
Jasmine <jasminerojas102@gmail.com>
2019-11-02 13:19:16 -0400

    add handleDelete fetch function

--- front/src/components/Start/start.js
+++ front/src/components/Start/start.js
@@ -26,6 +26,23 @@ export default class Start extends React.Component {
             .catch((err)=>{console.log(err)})
             console.log(this.state);
     }
+    handleDelete(event) {
+        event.preventDefault();
+        fetch('http://localhost:8080/resturant/one/' + event.currentTarget.value +'/', {
+            method: 'Delete',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(this.state)
+        })
+        .then((rep)=>{return rep.json()})
+        .then((res)=>{console.log(res)
+            localStorage.setItem('token', res.token);
+            this.props.history.push('/start');
+        })
+        .catch((err)=>{console.log(err)})
+        console.log(this.state);
+    }
 
     render () {
 
@@ -42,10 +59,11 @@ export default class Start extends React.Component {
                             <li className="location-wrapper">
                                 <Location
                                     title={resturant.title}
+                                    CafeImage={resturant.CafeImage}
                                     address={resturant.address}
-                                    comment={resturant.comments}
+                                    comment={resturant.comment}
                                 />
-                                  <Button className="deleteBtn"type="submit">delete</Button>
+                                  <Button className="deleteBtn"type="submit" onClick={(event)=> this.handleDelete(event)}>delete</Button>
                             </li>
                             )
                         }) 
