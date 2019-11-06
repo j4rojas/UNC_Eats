95c87215656f2ded65534a8e67737325a9b10461
Jasmine <jasminerojas102@gmail.com>
2019-11-06 18:49:14 -0500

    remove extra location tests

--- front/src/components/Location/Location.test.js
+++ front/src/components/Location/Location.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {Location} from './Location';
-import Comment from '../Comment/Comment';
+import {Comments} from '../Comment/Comment';
 import {addComment} from '../../actions';
 
 describe('<Location/>', () => {
@@ -9,7 +9,7 @@ describe('<Location/>', () => {
     beforeAll(() => {
         for (let i = 0; i < 10; i++) {
             seedComments.push({
-                text: `Comment ${i}`
+                text: `Comments ${i}`
             });
         }
     });
@@ -22,25 +22,5 @@ describe('<Location/>', () => {
         const title = 'Foo';
         const wrapper = shallow(<Location title={title} comments={[]} />);
         expect(wrapper.contains(<h3>{title}</h3>)).toEqual(true);
-    });
-
-    it('Dispatches addComment from addComment', () => {
-        const dispatch = jest.fn();
-        const index = 2;
-        const wrapper = shallow(
-            <Location comments={[]} index={index} dispatch={dispatch} />
-        );
-        const instance = wrapper.instance();
-        const text = seedComments[0].text;
-        instance.addComment(text);
-        expect(dispatch).toHaveBeenCalledWith(addComment(text, index));
-    });
-
-    it('Renders the comments', () => {
-        const wrapper = shallow(<Location comments={seedComments} />);
-        const comments = wrapper.find(Comment);
-        expect(comments.length).toEqual(seedComments.length);
-        const firstComment = comments.first();
-        expect(firstComment.prop('text')).toEqual(seedComments[0].text);
     });
 });
