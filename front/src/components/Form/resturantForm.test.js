d5ea85d796ba9a682249a7e5964149b2e76ccccd
Jasmine <jasminerojas102@gmail.com>
2019-11-06 20:12:55 -0500

    add enzyme to resturant test

--- front/src/components/Form/resturantForm.test.js
+++ front/src/components/Form/resturantForm.test.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
-import resturantForm from '../Form/resturantForm';
-import Button from 'react-bootstrap/Button';
+import {shallow} from 'enzyme';
 
 describe('<resturantForm />', () => {
     it('Renders without crashing', () => {
         shallow(<resturantForm/>);
     });
-
-    it('Renders the create button initially', () => {
-        const wrapper = shallow(<resturantForm />);
-        expect(wrapper.hasClass('createBtn')).toEqual(true);
-    });
+    
 });
